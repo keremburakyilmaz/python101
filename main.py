b6ffa89491597c41d98391c49230512968f9e95c
Kerem Burak Yılmaz <kyilmaz22@ku.edu.tr>
2023-03-09 15:42:15 +0300

    Introduction to Python and Github Basics

---
+++ main.py
@@ -0,0 +1,16 @@
+# original_price = int(input("please enter the original price: "))
+# new_price = original_price * 8 / 10
+#
+# print("new price:", new_price)
+
+
+# first_score = int(input("First score?: "))
+# second_score = int(input("Second score?: "))
+# third_score = int(input("Third score?: "))
+# average_score = int((first_score + second_score + third_score) / 3)
+# print("Average score:", average_score)
+
+
+# interest_rate = int(input("Annual interest rate?: "))
+# p = 10000/((1+interest_rate/100)**10)
+# print(int(p))
